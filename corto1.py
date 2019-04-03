530e10d519080c13fb2ee625022d0aa9b3fd7792
Cpzty <Cristianpzty@gmail.com>
2019-04-02 18:36:45 -0600

    derivada de J

---
+++ corto1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  2 18:33:06 2019
+
+@author: Cristian
+"""
+
+def derivada_gradiente(x,y,teta):
+    m,n = x.shape
+    h = np.matmul(x,teta)
+    for i in range(m):
+        cost = (h-y) * x[m,m]
+    return cost    
